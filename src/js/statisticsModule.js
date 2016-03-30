837518b4ca105b90da5885c9529445a7febede11
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-03-30 07:58:08 -0300

    Now only removes trailling slash character if it actually is a trailing slash

--- src/js/statisticsModule.js
+++ src/js/statisticsModule.js
@@ -5,8 +5,12 @@ CoreLibrary.statisticsModule = (function () {
       getStatistics: function ( type, filter ) {
          // Remove url parameters from filter
          filter = filter.match(/[^?]*/)[0];
+
          // Remove trailing slash
-         filter = filter.slice(0, -1);
+         if (filter[filter.length - 1] === '/') {
+            filter = filter.slice(0, -1);
+         }
+
          var baseApiUrl = 'https://api.kambi.com/statistics/api/';
          console.debug(baseApiUrl + CoreLibrary.config.offering + '/' + type + '/' + filter + '.json');
          return CoreLibrary.getData(baseApiUrl + CoreLibrary.config.offering + '/' + type + '/' + filter + '.json');
