2cd830fc0072fc7c4b0f7a459441fca1faace468
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2017-01-10 16:58:30 +0100

    added ES6 Symbol and Array.findIndex polyfills

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,10 +1,16 @@
-import es6Promise from 'es6-promise'; // ES6 Promise polyfill
+// ES6 Promise polyfill
+import es6Promise from 'es6-promise';
+
+// ES6 Symbol polyfill, this polyfill is required to use for (a of b)
+import 'es6-symbol/implement'; // the '/implement' activates the polyfill automatically
+
+/* eslint-disable no-extend-native */
 
 /*
 File with polyfills we use in this project
 */
 
-es6Promise.polyfill();
+es6Promise.polyfill(); // activates the polyfill
 
 // Object.assign polyfill
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
@@ -35,28 +41,77 @@ if (typeof Object.assign != 'function') {
 // Array.find polyfill
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/find
 if (!Array.prototype.find) {
-   // eslint-disable-next-line
-   Array.prototype.find = function(predicate) {
-      if (this == null) {
-         throw new TypeError('Array.prototype.find called on null or undefined');
-      }
-      if (typeof predicate !== 'function') {
-         throw new TypeError('predicate must be a function');
-      }
-      var list = Object(this);
-      // eslint-disable-next-line
-      var length = list.length >>> 0;
-      var thisArg = arguments[1];
-      var value;
-
-      for (var i = 0; i < length; i++) {
-         value = list[i];
-         if (predicate.call(thisArg, value, i, list)) {
-            return value;
+   Object.defineProperty(Array.prototype, 'find', {
+      enumerable: false,
+      value: function(predicate) {
+         if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
          }
+         if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+         }
+         var list = Object(this);
+         // eslint-disable-next-line
+         var length = list.length >>> 0;
+         var thisArg = arguments[1];
+         var value;
+
+         for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+               return value;
+            }
+         }
+         return undefined;
       }
-      return undefined;
-   };
+   });
+}
+
+// Array.findIndex polyfill
+// https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
+if (!Array.prototype.findIndex) {
+   Object.defineProperty(Array.prototype, 'findIndex', {
+      enumerable: false,
+      value: function(predicate) {
+         // 1. Let O be ? ToObject(this value).
+         if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+         }
+
+         var o = Object(this);
+
+         // 2. Let len be ? ToLength(? Get(O, "length")).
+         var len = o.length >>> 0; // eslint-disable-line
+
+         // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+         if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+         }
+
+         // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+         var thisArg = arguments[1];
+
+         // 5. Let k be 0.
+         var k = 0;
+
+         // 6. Repeat, while k < len
+         while (k < len) {
+            // a. Let Pk be ! ToString(k).
+            // b. Let kValue be ? Get(O, Pk).
+            // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+            // d. If testResult is true, return k.
+            var kValue = o[k];
+            if (predicate.call(thisArg, kValue, k, o)) {
+               return k;
+            }
+            // e. Increase k by 1.
+            k++;
+         }
+
+         // 7. Return -1.
+         return -1;
+      }
+   });
 }
 
 // The Number.isNaN() method determines whether the passed value is NaN. It is a more robust version of the original, global isNaN().
