45f07d2f4605a0df582f0834a59830603cc71b9e
Owain <owain@Owains-MacBook-Pro.local>
2017-12-13 11:54:45 +0100

    updated widget.css and widget-api.js version. Removed kambi- prefix in the font family declarations

--- src/constants.js
+++ src/constants.js
@@ -12,9 +12,9 @@ module.exports = {
 
    // version of the widget API to use
    // https://c3-static.kambi.com/sb-mobileclient/widget-api/{widgetApiVersion}/kambi-widget-api.js
-   widgetApiVersion: '1.0.0.32',
+   widgetApiVersion: "1.0.0.48",
 
    // version of widgets.css to use
    // https://c3-static.kambi.com/sb-mobileclient/widget-api/{widgetCssVersion}/resources/css/kambi/kambi/widgets.css
-   widgetCssVersion: '1.0.0.45'
+   widgetCssVersion: "1.0.0.48"
 };
