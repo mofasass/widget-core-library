89069764f6926b08ecf066707de8ba7a8ce33cc9
danielhoffmann <danielhoffmann@globalmouth.com>
2018-01-26 14:31:24 +0100

    updated package-lock and constants.js files

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,8 @@
 // The constants defined here are used by both the
 // core-library and the build-tools
-// don't use "export default" here because inside the build-tools there is no transpiling
 
 module.exports = {
-   // version of the widget API to use
-   // https://c3-static.kambi.com/sb-mobileclient/widget-api/{widgetApiVersion}/kambi-widget-api.js
    // version of widgets.css to use
    // https://c3-static.kambi.com/sb-mobileclient/widget-api/{widgetCssVersion}/resources/css/kambi/kambi/widgets.css
-   widgetCssVersion: "1.0.0.48"
+   widgetCssVersion: "1.0.0.55"
 };
