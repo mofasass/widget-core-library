bb8292eacf26c96d8912bc432c574d76887f22cf
angelicahl88 <angelica@globalmouth.com>
2018-03-21 14:11:52 +0100

    updated css version to 57

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
   // version of widgets.css to use
   // https://c3-static.kambi.com/sb-mobileclient/widget-api/{widgetCssVersion}/resources/css/kambi/kambi/widgets.css
-  widgetCssVersion: '1.0.0.56',
+  widgetCssVersion: '1.0.0.57',
 }
